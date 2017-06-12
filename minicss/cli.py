201413967c3d33c1c3cc9127b8003173031186ee
Yochem <6283@pierson.nl>
2017-06-12 20:15:22 +0200

    initial commit

---
+++ minicss/cli.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import sys
+import requests
+import click
+
+@click.command()
+@click.version_option('1.0')
+@click.argument('css_file', type=click.Path(exists=True))
+def main(css_file):
+
+    # Grab the file contents
+    with open(css_file, 'r') as c:
+        css = c.read()
+
+    # Pack it, ship it
+    payload = {'input': css}
+    url = 'https://cssminifier.com/raw'
+    click.secho("Requesting mini-me of {}. . .".format(c.name), fg='green', bold=True)
+    r = requests.post(url, payload)
+
+    # Write out minified version
+    minified = css_file.rstrip('.css')+'.min.css'
+    with open(minified, 'w') as m:
+        m.write(r.text)
+
+    click.secho("Minification complete. See {}".format(m.name), fg='green', bold=True)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+dependencies = ['click']
+
+with open('README.md') as r:
+    long_description = r.read()
+
+with open('LICENSE') as l:
+    license = l.read()
+
+setup(
+    name = 'mini-css',
+    packages = ['mini-css'],
+    version = '1.0',
+    description = long_description,
+    long_description = long_description,
+    author = 'Yochem van Rosmalen',
+    author_email = 'yochem@icloud.com',
+    #url = 'https://github.com/yochem/',
+    license = license,
+    include_package_data = True,
+    zip_safe = False,
+    platforms = 'any',
+    install_requires = dependencies,
+    entry_points = {
+        'console_scripts': [
+            'mini=minicss.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
